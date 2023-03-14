3358ae81c16f1205133ca154e6bc12df4fe2653f
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-14 19:23:48 +0600

    Модели зарегистрированы в админке

--- marketapp/admin.py
+++ marketapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
+from marketapp.models import ProductInCart, Order
+from marketapp.models.orders import OrderProduct
 from marketapp.models import Product
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(ProductInCart)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
