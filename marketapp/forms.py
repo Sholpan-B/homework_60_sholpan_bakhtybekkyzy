e705648e8aacf06b6b066742c8cbfbb72827495f
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-03-14 19:25:15 +0600

    Добавлены шаблоны

--- marketapp/forms.py
+++ marketapp/forms.py
@@ -1,34 +1,45 @@
+from decimal import Decimal
+
 from django import forms
 from django.core.exceptions import ValidationError
-from django.forms import widgets
 
-from marketapp.models import StatusChoice
+from marketapp.models import Product
 
 
-class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label="Наименование")
-    description = forms.CharField(max_length=2000, required=False, label="Описание", widget=widgets.Textarea)
-    image = forms.CharField(max_length=3000, required=False, label="Фото")
-    category = forms.ChoiceField(choices=StatusChoice.choices, required=True, label="Категория")
-    in_stock = forms.IntegerField(required=True, label='Остаток')
-    price = forms.DecimalField(required=True, min_value=1, max_digits=7, decimal_places=2, label='Стоимость')
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'image', 'category', 'in_stock', 'price')
+        labels = {
+            'name': 'Наименование',
+            'description': 'Описание',
+            'image': 'Фото',
+            'category': 'Категория',
+            'in_stock': 'Остаток',
+            'price': 'Стоимость'
+        }
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
-        if len(name) < 3:
-            raise ValidationError('Имя должно быть длиннее 3 символов!')
+        if len(name) > 200:
+            raise ValidationError('Длина превышает 200 символов!')
         return name
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if "@" not in email:
-            raise ValidationError("Email должен содержать '@'!")
-        return email
+    def clean_in_stock(self):
+        in_stock = self.cleaned_data.get('in_stock')
+        if in_stock < 0:
+            raise ValidationError('Остаток не может быть отрицательным')
+        return in_stock
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price < Decimal('0.00'):
+            raise ValidationError('Стоимость не может быть отрицательной')
+        return price
 
 
 class SearchForm(forms.Form):
-    search = forms.CharField(
-        label="Поиск",
-        max_length=300,
-        required=False,
-    )
+    search = forms.CharField(max_length=20, required=False, label='Найти')
+
+
+
--- marketapp/urls.py
+++ marketapp/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
-from marketapp.views import index_view, add_view, edit_view, delete_view, confirm_delete, search_view, detail_view
+from marketapp.views.base import IndexView, IndexRedirectView
+from marketapp.views.carts import add_to_cart, cart, remove_from_cart
+from marketapp.views.orders import checkout_view
+from marketapp.views.products import ProductDetail, ProductAddView, ProductUpdateView, ProductDeleteView
 
 urlpatterns = [
-    path("", index_view, name='index'),
-    path("product/", index_view, name='index'),
-    path("product/add", add_view, name='product_add'),
-    path('product/<int:pk>', detail_view, name='product_detail'),
-    path('product/edit/<int:pk>', edit_view, name='product_edit'),
-    path('product/delete/<int:pk>', delete_view, name='product_delete'),
-    path('product/confirm_delete/<int:pk>', confirm_delete, name='confirm_delete'),
-    path("product/search", search_view, name='search'),
+    path("", IndexView.as_view(), name='index'),
+    path("product/", IndexRedirectView.as_view(), name='product_index_redirect'),
+    path("product/add", ProductAddView.as_view(), name='product_add'),
+    path('product/<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('product/edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product/confirm_delete/<int:pk>/', ProductDeleteView.as_view(), name='confirm_delete'),
+    path('cart/add_product/<int:pk>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart, name='cart'),
+    path('cart/remove_from_cart/<int:pk>/', remove_from_cart, name='remove_from_cart'),
+    path('cart/order/', checkout_view, name='checkout'),
 ]
